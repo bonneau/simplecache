e0bbcd82f7db30397224b3f14d9e118b49487e4b
bonneau <scott.bonneau@gmail.com>
2014-07-07 17:01:59 -0700

    Added concurrency.

--- simplecache.py
+++ simplecache.py
@@ -1,44 +1,66 @@
 import time
 
 from collections import OrderedDict
+from threading import Condition
 
 
 class SimpleCache:
     def __init__(self, timeout=None, max_items=None):
+        self._c = Condition()
         self._d = OrderedDict()
         self._timeout = timeout
         self._max_items = max_items
 
     def __len__(self):
-        return len(self._d)
+        self._c.acquire()
+        try:
+            return len(self._d)
+        finally:
+            self._c.release()
 
     def __contains__(self, k):
-        if k in self._d:
-            v, t = self._d[k]
-            self.__expire_if_necessary(k, t)
-            return k in self._d
-        else:
-            return False
+        self._c.acquire()
+        try:
+            if k in self._d:
+                v, t = self._d[k]
+                self.__expire_if_necessary(k, t)
+                return k in self._d
+            else:
+                return False
+        finally:
+            self._c.release()
 
     def __getitem__(self, k):
-        v, t = self._d[k]
-        self.__expire_if_necessary(k, t)
+        self._c.acquire()
+        try:
+            v, t = self._d[k]
+            self.__expire_if_necessary(k, t)
 
-        # Delete and re-add; read reference resets expiration
-        del self._d[k]
-        self[k] = v
+            # Delete and re-add; read reference resets expiration
+            del self._d[k]
+            self[k] = v
 
-        return v
+            return v
+        finally:
+            self._c.release()
 
     def __setitem__(self, k, v):
-        timeout = None
-        if self._timeout:
-            timeout = time.time() + self._timeout
-        self._d[k] = (v, timeout)
-        self.__prune_if_necessary()
+        self._c.acquire()
+        try:
+            timeout = None
+            if self._timeout:
+                timeout = time.time() + self._timeout
+            self._d[k] = (v, timeout)
+            self.__prune_if_necessary()
+        finally:
+            self._c.release()
 
     def __delitem__(self, k):
-        del self._d[k]
+        self._c.acquire()
+        try:
+            del self._d[k]
+        finally:
+            self._c.release()
 
     def __iter___(self):
         # TODO: Implement
