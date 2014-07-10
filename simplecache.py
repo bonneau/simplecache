1d28b9902ef5b47ced9146e5a3c92e85ae1fe82f
bonneau <scott.bonneau@gmail.com>
2014-07-10 13:48:48 -0700

    Pulled out thread safety into its own class, ThreadSafeSimpleCache.

--- simplecache.py
+++ simplecache.py
@@ -4,65 +4,44 @@ from collections import OrderedDict
 from threading import Condition
 
 
-class SimpleCache:
+class SimpleCache(object):
     def __init__(self, timeout=None, max_items=None):
-        self._c = Condition()
         self._d = OrderedDict()
         self._timeout = timeout
         self._max_items = max_items
 
     def __len__(self):
-        self._c.acquire()
-        try:
-            return len(self._d)
-        finally:
-            self._c.release()
+        return len(self._d)
 
     def __contains__(self, k):
-        self._c.acquire()
-        try:
-            if k in self._d:
-                v, t = self._d[k]
-                self.__expire_if_necessary(k, t)
-                return k in self._d
-            else:
-                return False
-        finally:
-            self._c.release()
-
-    def __getitem__(self, k):
-        self._c.acquire()
-        try:
+        if k in self._d:
             v, t = self._d[k]
             self.__expire_if_necessary(k, t)
+            return k in self._d
+        else:
+            return False
 
-            # Delete and re-add; read reference resets expiration
-            del self._d[k]
-            self[k] = v
+    def __getitem__(self, k):
+        v, t = self._d[k]
+        self.__expire_if_necessary(k, t)
 
-            return v
-        finally:
-            self._c.release()
+        # Delete and re-add; read reference resets expiration
+        del self._d[k]
+        self[k] = v
+
+        return v
 
     def __setitem__(self, k, v):
-        self._c.acquire()
-        try:
-            timeout = None
-            if self._timeout:
-                timeout = time.time() + self._timeout
-            self._d[k] = (v, timeout)
-            self.__prune_if_necessary()
-        finally:
-            self._c.release()
+        timeout = None
+        if self._timeout:
+            timeout = time.time() + self._timeout
+        self._d[k] = (v, timeout)
+        self.__prune_if_necessary()
 
     def __delitem__(self, k):
-        self._c.acquire()
-        try:
-            del self._d[k]
-        finally:
-            self._c.release()
+        del self._d[k]
 
-    def __iter___(self):
+    def __iter__(self):
         # TODO: Implement
         pass
 
@@ -74,3 +53,51 @@ class SimpleCache:
     def __prune_if_necessary(self):
         if self._max_items and len(self._d) > self._max_items:
             self._d.popitem(last=False)
+
+
+class ThreadSafeSimpleCache(SimpleCache):
+    def __init__(self):
+        super(ThreadSafeSimpleCache, self).__init__()
+        self._c = Condition()
+
+    def __len__(self):
+        self._c.acquire()
+        try:
+            return SimpleCache.__len__(self)
+        finally:
+            self._c.release()
+
+    def __contains__(self, k):
+        self._c.acquire()
+        try:
+            return SimpleCache.__contains__(self, k)
+        finally:
+            self._c.release()
+
+    def __getitem__(self, k):
+        self._c.acquire()
+        try:
+            return SimpleCache.__getitem__(self, k)
+        finally:
+            self._c.release()
+
+    def __setitem__(self, k, v):
+        self._c.acquire()
+        try:
+            SimpleCache.__setitem__(self, k, v)
+        finally:
+            self._c.release()
+
+    def __delitem__(self, k):
+        self._c.acquire()
+        try:
+            SimpleCache.__delitem__(self, k)
+        finally:
+            self._c.release()
+
+    def __iter__(self):
+        self._c.acquire()
+        try:
+            return SimpleCache.__iter__(self)
+        finally:
+            self._c.release()
